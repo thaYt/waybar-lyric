1bdde48735ff2daa38b5043676137dd1617f0c2c
Ephemeral <theephemeral.txt@gmail.com>
2025-04-27 21:41:44 +0600

    chore: rename `TootlipColor` -> `TooltipColor`

--- cli.go
+++ cli.go
@@ -18,7 +18,7 @@ var (
 	VerboseLog    = false
 	MaxTextLength = 150
 	TooltipLines  = 8
-	TootlipColor  = "#cccccc"
+	TooltipColor  = "#cccccc"
 	LogFilePath   = ""
 )
 
@@ -28,7 +28,7 @@ func init() {
 	pflag.BoolVar(&ToggleState, "toggle", ToggleState, "Toggle player state (pause/resume)")
 	pflag.IntVar(&MaxTextLength, "max-length", MaxTextLength, "Maximum length of lyrics text")
 	pflag.IntVar(&TooltipLines, "tooltip-lines", TooltipLines, "Maximum lines of waybar tooltip")
-	pflag.StringVarP(&TootlipColor, "tooltip-color", "t", TootlipColor, "Color of inactive lyrics lines")
+	pflag.StringVarP(&TooltipColor, "tooltip-color", "t", TooltipColor, "Color of inactive lyrics lines")
 	pflag.BoolVarP(&VerboseLog, "verbose", "v", VerboseLog, "Use verbose logging")
 	pflag.StringVar(&LogFilePath, "log-file", LogFilePath, "File where logs should be saved")
 
