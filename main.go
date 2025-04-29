df514dde5bd9e7a10a41f6c510dc0f246cdbb41e
Ephemeral <theephemeral.txt@gmail.com>
2025-04-29 23:19:41 +0600

    chore: bump version v0.9.0 -> v0.10.0

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	SleepTime = 500 * time.Millisecond
-	Version   = "waybar-lyric v0.9.0 (https://github.com/Nadim147c/waybar-lyric)"
+	Version   = "waybar-lyric v0.10.0 (https://github.com/Nadim147c/waybar-lyric)"
 )
 
 func truncate(input string) string {
